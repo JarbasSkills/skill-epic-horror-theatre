d8cc7952bee27e7eeeeed5a3ed16a0f01c84569e
jarbasal <jarbasai@mailfence.com>
2020-08-18 04:03:58 +0100

    #0

---
+++ README.md
@@ -0,0 +1,37 @@
+# <img src='./res/icon/icon.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/> Epic Horror Theatre Skill 
+
+Radio drama adaptions of H. P. Lovecraft
+
+## About 
+
+![](./ui/logo.png)
+
+This skill brings to you:
+
+- a radio drama adaptation of H.P. Lovecraft's classic tale, "The Color Out Of Space" directed by Ron N. Butler and published by the Atlanta Radio Theatre Company! Written in March 1927, tonight's tale details the misadventures of a surveyor from Boston who goes to Arkham, Massachusetts to learn the truth behind a shunned place the locals call "the blasted heath"
+
+- a radio drama adaptation of H.P. Lovecraft's classic and titular tale, At The Mountains of Madness! Written in 1931 and finally serialized in 1936, today's horrifying tale follows a team of archaeologists, led by Professor William Dyer from Miskatonic University, as they go to Antarctica and discover secrets about the history of life on Earth that are as mind-boggling as they are terrifying.
+
+- a radio drama adaptation of H.P. Lovecraft's the eponymous tale, "The Shadow Over Innsmouth", written by the legendary O.G. of horror H.P. Lovecraft in late 1931 and once again adapted to audio by the Atlanta Theater Radio Company. In tonight's tale, we follow a youth on his coming-of-age tour through New England, where on his antiquarian adventures, he hears stories of a little town in Massachusetts clouded with rumors of pirate treasure, vicious plagues, and its strange-looking furtive people. The truth, however, is more personal and much more horrifying than our narrator could dare to fear it could possibly be...
+
+## Examples 
+
+* "read the shadow over innsmouth"
+* "play mountains of madness book"
+* "read the color out of space"
+* "read lovecraft"
+
+## Credits 
+- JarbasAl
+- [Epic Horror Theatre](https://www.youtube.com/channel/UCt3yjI_o-5HBwwyuUDNB65w)
+
+
+## Category
+**Entertainment**
+
+## Tags
+#audio 
+#books
+#radio theatre
+#lovecraft
+#entertainment
---
+++ __init__.py
@@ -0,0 +1,107 @@
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel, CPSMatchType
+import random
+import re
+from os.path import join, dirname
+
+
+class EpicHorrorTheatreSkill(CommonPlaySkill):
+
+    def __init__(self):
+        super().__init__("Epic Horror Theatre")
+        self.supported_media = [CPSMatchType.GENERIC,
+                                CPSMatchType.AUDIOBOOK]
+
+    def initialize(self):
+        self.add_event('skill-epic-horror-theatre.jarbasskills.home',
+                       self.handle_homescreen)
+
+    def get_intro_message(self):
+        self.speak_dialog("intro")
+        self.gui.show_image(self.logo)
+
+    # homescreen
+    def handle_homescreen(self, message):
+        pass  # TODO selection menu
+
+    # common play
+    def remove_voc(self, utt, voc_filename, lang=None):
+        lang = lang or self.lang
+        cache_key = lang + voc_filename
+
+        if cache_key not in self.voc_match_cache:
+            self.voc_match(utt, voc_filename, lang)
+
+        if utt:
+            # Check for matches against complete words
+            for i in self.voc_match_cache[cache_key]:
+                # Substitute only whole words matching the token
+                utt = re.sub(r'\b' + i + r"\b", "", utt)
+
+        return utt
+
+    def clean_vocs(self, phrase):
+        phrase = self.remove_voc(phrase, "reading")
+        phrase = self.remove_voc(phrase, "lovecraft")
+        phrase = self.remove_voc(phrase, "atlanta")
+        phrase = self.remove_voc(phrase, "epic_horror")
+        phrase = phrase.strip()
+        return phrase
+
+    def CPS_match_query_phrase(self, phrase, media_type):
+        media_path = join(dirname(__file__), "media")
+        original = phrase
+        match = None
+
+        score = 0
+
+        if media_type == CPSMatchType.AUDIOBOOK:
+            score += 0.1
+            match = CPSMatchLevel.GENERIC
+
+        if self.voc_match(original, "atlanta"):
+            score += 0.15
+            match = CPSMatchLevel.CATEGORY
+
+        if self.voc_match(original, "epic_horror"):
+            score += 0.15
+            match = CPSMatchLevel.CATEGORY
+
+        if self.voc_match(original, "lovecraft"):
+            score += 0.5
+            match = CPSMatchLevel.ARTIST
+
+        phrase = self.clean_vocs(phrase)
+
+        if self.voc_match(phrase, "color_out_of_space"):
+            score += 0.7
+            url = join(media_path, "The Color Out of Space.mp3")
+        elif self.voc_match(phrase, "innsmouth"):
+            score += 0.7
+            url = join(media_path, "The Shadow Over Innsmouth.mp3")
+        elif self.voc_match(phrase, "mountains_of_madness"):
+            score += 0.7
+            url = join(media_path, "At The Mountains of Madness.mp3")
+        else:
+            url = join(media_path, random.choice(
+                ["At The Mountains of Madness.mp3",
+                 "The Shadow Over Innsmouth.mp3",
+                 "The Color Out of Space.mp3"
+                 ]))
+
+        if score >= 0.9:
+            match = CPSMatchLevel.EXACT
+        elif score >= 0.7:
+            match = CPSMatchLevel.ARTIST
+        elif score >= 0.5:
+            match = CPSMatchLevel.CATEGORY
+
+        if match is not None:
+            return (phrase, match, {"stream": url})
+        return None
+
+    def CPS_start(self, phrase, data):
+        self.CPS_play(data["stream"])
+
+
+def create_skill():
+    return EpicHorrorTheatreSkill()
